34eb40d160ad5a26db4e9f28551cda43910cb79d
Ezequiel Ramos <ezequielmr94@gmail.com>
2019-03-09 13:36:54 -0300

    using public and private keys to encrypt and decrypt data

--- server.py
+++ server.py
@@ -1,5 +1,28 @@
 from Crypto.PublicKey import RSA
 from Crypto import Random
+from Crypto.Cipher import PKCS1_OAEP
+
 random_generator = Random.new().read
-key = RSA.generate(1024, random_generator)
-print(key)
+private_key = RSA.generate(1024, random_generator)
+public_key = private_key.publickey()
+
+print(private_key.exportKey())
+print()
+print(public_key.exportKey())
+
+print()
+print("-" * 35)
+print()
+
+public_cipher_rsa = PKCS1_OAEP.new(public_key)
+enc_session_key = public_cipher_rsa.encrypt(b"uma informacao confidencial")
+
+print('dado encriptado')
+print(enc_session_key)
+
+print()
+private_cipher_rsa = PKCS1_OAEP.new(private_key)
+session_key = private_cipher_rsa.decrypt(enc_session_key)
+
+print('dado decriptado')
+print(session_key)
